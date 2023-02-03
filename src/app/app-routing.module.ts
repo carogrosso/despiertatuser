74a4075f6ce7b99d6f6df622bfbe41bba60a5e34
Caro <grossocrln@gmail.com>
2023-02-03 16:54:08 -0300

    commit final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogComponent } from './components/blog/blog.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { SobremiComponent } from './components/sobremi/sobremi.component';
+import { TalleresComponent } from './components/talleres/talleres.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "servicios", component: ServiciosComponent},
+  {path: "talleres", component: TalleresComponent},
+  {path: "blog", component: BlogComponent},
+  {path: "contacto", component: ContactoComponent},
+  {path: "sobremi", component: SobremiComponent},
+  {path: "productos", component: ProductosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SobremiComponent } from './components/sobremi/sobremi.component';
+import { TalleresComponent } from './components/talleres/talleres.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProductosComponent } from './components/productos/productos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SobremiComponent,
+    TalleresComponent,
+    BlogComponent,
+    HeaderComponent,
+    ServiciosComponent,
+    ContactoComponent,
+    FooterComponent,
+    ProductosComponent
   ],
   imports: [
     BrowserModule,
